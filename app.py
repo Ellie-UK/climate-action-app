337f81062812b37723e936e6ef26e1ad3edc5de8
ellie-uk <c0052323@newcastle.ac.uk>
2021-11-22 14:38:47 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+# IMPORTS
+import socket
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    my_host = "127.0.0.1"
+    free_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    free_socket.bind((my_host, 0))
+    free_socket.listen(5)
+    free_port = free_socket.getsockname()[1]
+    free_socket.close()
+
+    app.run(host=my_host, port=free_port, debug=True)
