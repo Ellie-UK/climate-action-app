b9c09536f4c9db9183c323b804734a9fe9e78dbb
Jun <j.zhao1@newcastle.ac.uk>
2021-12-07 14:14:18 +0000

    Linked the login to users/views.py

--- users/views.py
+++ users/views.py
@@ -1,9 +1,15 @@
 from flask import Blueprint, render_template
 
+from users.forms import LoginForm
+
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
 
 
 # view user login
 @users_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        return login()
+    return render_template('login.html', form=form)
