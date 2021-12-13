ec5e9c9ff4c3625112527df9165a9be81cc0acb3
Ethan <e.wilson17@newcastle.ac.uk>
2021-12-13 14:31:52 +0000

    Created Register function and linked it to register page, included basic validation to avoid repeat emails

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
-
-from users.forms import LoginForm
+from flask import Blueprint, render_template, flash, redirect, url_for
+from users.forms import LoginForm, RegisterForm
+from models import User
+from app import db
 
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
 
@@ -13,3 +14,36 @@ def login():
     if form.validate_on_submit():
         return login()
     return render_template('login.html', form=form)
+
+@users_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    # if request method is POST or form is valid
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        # if this returns a user, then the email already exists in database
+
+        # if email already exists redirect user back to signup page with error message so user can try again
+        if user:
+            flash('Email address already exists')
+            return render_template('register.html', form=form)
+
+        # create a new user with the form data
+        new_user = User(email=form.email.data,
+                        firstname=form.firstname.data,
+                        lastname=form.lastname.data,
+                        phone=form.phone.data,
+                        password=form.password.data,
+                        pin_key=form.pin_key.data,
+                        role='user')
+
+        # add the new user to the database
+        db.session.add(new_user)
+        db.session.commit()
+
+        # sends user to login page
+        return redirect(url_for('users.login'))
+
+    return render_template('register.html', form=form)
+
