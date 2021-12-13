2f7e4a153f11c8e59418222198d6ee270aef0f0c
Torac <H.Torac2@newcastle.ac.uk>
2021-12-13 17:39:54 +0100

    Added the functionality to view all registered users on admin page.

--- admin/views.py
+++ admin/views.py
@@ -1,10 +1,19 @@
 from flask import Blueprint, render_template
+from models import User
 
 admin_blueprint = Blueprint('admin', __name__, template_folder='templates')
 
 
 # view admin page
 @admin_blueprint.route('/admin')
-# add access restrictions
+# MISSING - add access restrictions
 def admin():
     return render_template('admin.html', lastname="PLACEHOLDER FOR LASTNAME")
+
+
+# view all registered users
+@admin_blueprint.route('/view_users', methods=['POST'])
+# MISSING - add access restrictions
+def view_users():
+    return render_template('admin.html', lastname="PLACEHOLDER FOR LASTNAME",
+                           all_users=User.query.filter_by(role='user').all())
