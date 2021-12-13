c17d915530f2f4c77208467deeb454d54ecf3bdb
Jun <j.zhao1@newcastle.ac.uk>
2021-12-13 14:15:49 +0000

    Added a condition to check for the users login email and password

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
+from werkzeug.security import check_password_hash
 
+from models import User
 from users.forms import LoginForm
 
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
@@ -9,7 +11,12 @@ users_blueprint = Blueprint('users', __name__, template_folder='templates')
 @users_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-
+    # Validation check for email and password
     if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if not user or not check_password_hash(user.password, form.password.data):
+            flash('Please check your login details and try again')
+
         return login()
     return render_template('login.html', form=form)
