4b429f28ca984b6f8635525d11ca198a50572b42
Jun <j.zhao1@newcastle.ac.uk>
2021-12-14 14:49:11 +0000

    implmeneted users activity monitor for last logged in and current logged in is using the datetime.now function to find current date and time.

--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, render_template, flash
+from datetime import datetime
+
+from flask import Blueprint, render_template, flash, url_for, redirect
+from flask_login import login_user
 from werkzeug.security import check_password_hash
 
+from app import db
 from models import User
-from users.forms import LoginForm
+from users.forms import LoginForm, RegisterForm
 
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
 
@@ -11,12 +15,47 @@ users_blueprint = Blueprint('users', __name__, template_folder='templates')
 @users_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    # Validation check for email and password
+
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
 
         if not user or not check_password_hash(user.password, form.password.data):
             flash('Please check your login details and try again')
 
+        login_user(user)
+
+        user.last_logged_in = user.current_logged_in
+        user.current_logged_in = datetime.now()
+        db.session.add(user)
+        db.session.commit()
+
         return login()
     return render_template('login.html', form=form)
+
+
+@users_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    # if request method is POST or form is valid
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        # if this returns a user, then the email already exists in database
+        # if email already exists redirect user back to signup page with error message so user can try again
+        if user:
+            flash('Email address already exists')
+            return render_template('register.html', form=form)
+        # create a new user with the form data
+        new_user = User(email=form.email.data,
+                        firstname=form.firstname.data,
+                        lastname=form.lastname.data,
+                        phone=form.phone.data,
+                        password=form.password.data,
+                        pin_key=form.pin_key.data,
+                        role='user')
+        # add the new user to the database
+        db.session.add(new_user)
+        db.session.commit()
+        # sends user to login page
+        return redirect(url_for('users.login'))
+
+    return render_template('register.html', form=form)
