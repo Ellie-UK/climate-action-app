af4b814368b025fa8bf8c0152868d2c6afbfe5e9
Ellie Smith <91325227+Ellie-UK@users.noreply.github.com>
2021-12-16 18:00:24 +0000

    Created ChangePassword form

--- users/forms.py
+++ users/forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import Required, Email, Length, EqualTo
 class LoginForm(FlaskForm):
     email = StringField(validators=[Required(), Email()]) #Need to implement error code in to login.html, for it to show error message when triggered
     password = PasswordField(validators=[Required()]) #Need to implement error code in to login.html, for it to show error message when triggered
-    pin = StringField(validators=[Required(), Length(min=6, max=6, message='Pin must be 6 digits long')]) #Need to implement error code in to login.html, for it to show error message when triggered
+    #pin = StringField(validators=[Required(), Length(min=6, max=6, message='Pin must be 6 digits long')]) #Need to implement error code in to login.html, for it to show error message when triggered
     submit = SubmitField()
 
 
@@ -18,4 +18,10 @@ class RegisterForm(FlaskForm):
     password = PasswordField(validators=[Required(), Length(min=6, max=12, message='Password must be between 6 and 12 characters in length.')])
     confirm_password = PasswordField(validators=[Required(), EqualTo('password', message='Both password fields must be equal!')])
     pin_key = StringField(validators=[Required(), Length(min=32, max=32, message='pin key must be 32 characters in length.')])
+    submit = SubmitField()
+
+class ChangePasswordForm(FlaskForm):
+    current_password = PasswordField(validators=[Required()])
+    new_password = PasswordField(validators=[Required(), Length(min=6, max=12, message='Password must be between 6 and 12 characters in length.')])
+    confirm_new_password = PasswordField(validators=[Required(), EqualTo('new_password', message='Both password fields must be equal!')])
     submit = SubmitField()
