b53fbad7adff4a0069240f5b158dbb0a109ee50d
Ellie S <c0052323@newcastle.ac.uk>
2022-01-11 02:42:05 +0000

    Update

--- app.py
+++ app.py
@@ -51,13 +51,6 @@ def index():
 
 
 if __name__ == '__main__':
-    my_host = "127.0.0.1"
-    free_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    free_socket.bind((my_host, 0))
-    free_socket.listen(5)
-    free_port = free_socket.getsockname()[1]
-    free_socket.close()
-
     login_manager = LoginManager()
     login_manager.login_view = 'users.login'
     login_manager.init_app(app)
@@ -79,4 +72,4 @@ if __name__ == '__main__':
     app.register_blueprint(users_blueprint)
     app.register_blueprint(admin_blueprint)
 
-    app.run(host=my_host, port=free_port, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
