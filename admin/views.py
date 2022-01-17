675ebc45228336401eff2d63d4f2f20ebb29981d
Torac <H.Torac2@newcastle.ac.uk>
2022-01-17 20:02:15 +0100

    Deleted admin functionality to view logging data

--- admin/views.py
+++ admin/views.py
@@ -27,6 +27,7 @@ def required_roles(*roles, source):
 
     return wrapper
 
+
 # view admin page
 @admin_blueprint.route('/admin', methods=['GET', 'POST'])
 @login_required
@@ -43,31 +44,6 @@ def view_users():
     return render_template('admin.html', lastname=current_user.lastname,
                            all_users=User.query.all())
 
-
-# view security logging data (last 15 log records)
-@admin_blueprint.route('/logging', methods=['POST'])
-@login_required
-@required_roles('admin', source='Admin Page - security log')
-def logging():
-    # read last 15 log records
-    with open("user_logs.log", "r") as f:
-        content = f.read().splitlines()[-15:]
-        content.reverse()
-
-    return render_template('admin.html', lastname=current_user.lastname, logs=content)
-
-
-# view whole security log
-@admin_blueprint.route('/seclog', methods=['POST'])
-@login_required
-@required_roles('admin', source='Admin Page - full security log')
-def full_logging():
-    # read whole log
-    with open("user_logs.log", "r") as f:
-        content = f.read().splitlines()
-        content.reverse()
-
-    return render_template('security_log.html', logs=content)
 
 # send newsletter
 @admin_blueprint.route('/send_newsletter', methods=['GET', 'POST'])
