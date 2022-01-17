5b9c09e4f1cd4fbe01678989cc5c4df2834d1d87
hugor <hugo.ross-wilson@outlook.com>
2022-01-17 16:14:36 +0000

    Add admin start to quiz_home.html

--- admin/forms.py
+++ admin/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import Required, Email, Length, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
 
 class SendNewsletter(FlaskForm):
-    subject = StringField(validators=[Required()])
-    body = StringField(validators=[Required()])
+    subject = StringField(validators=[DataRequired()])
+    body = StringField(validators=[DataRequired()])
 
     submit = SubmitField()
--- faq/forms.py
+++ faq/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
 
 
 class FAQFormQuestion(FlaskForm):
-    question = StringField(validators=[Required()])
+    question = StringField(validators=[DataRequired()])
     submit = SubmitField()
 
 
 class FAQFormAnswer(FlaskForm):
-    question = StringField(validators=[Required()])
-    answer = TextAreaField(validators=[Required()])
+    question = StringField(validators=[DataRequired()])
+    answer = TextAreaField(validators=[DataRequired()])
     original_question = ""
     submit = SubmitField()
