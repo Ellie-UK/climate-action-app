8327cf17ef2f0b0aff05c3d7e5d57af9ea5a744a
hugor <hugo.ross-wilson@outlook.com>
2022-01-19 13:25:30 +0000

    Add test_quiz
     - test that the question is shown on the quiz page

---
+++ testing/quiz_tests.py
@@ -0,0 +1,47 @@
+import unittest
+from flask_testing import TestCase
+
+from forum.views import delete, forum, update
+from models import db, Forum, Comments, Quiz
+from app import app, load_user
+from models import User
+import mock
+
+
+class BaseTestCase(TestCase):
+    ''' Base test'''
+
+    def create_app(self):
+        app.config.from_object('config.TestConfig')
+        return app
+
+    def setUp(self):
+        db.create_all()
+        db.session.add(Quiz(question='Will this pass?',
+                            option1='Yes',
+                            option2='No',
+                            option3='No',
+                            option4='No',
+                            answer=1))
+        db.session.add(User(email='Jerry@email.com',
+                            password='Password',
+                            pin_key='BFB5S34STBLZCOB22K6PPYDCMZMH46OJ',
+                            firstname='Jerry',
+                            lastname='Jones',
+                            phone='0191-123-4567',
+                            role='user'))
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+
+class TestQuiz(BaseTestCase):
+    @mock.patch('flask_login.utils._get_user')
+    def test_quiz(self, current_user):
+        user = User.query.get(1)
+        current_user.return_value = user
+        with self.client:
+            response = self.client.get('/quiz', follow_redirects=True)
+            self.assertIn(b'Will this pass?', response.data)
